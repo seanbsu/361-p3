217b9f6b6454070927d8a72c2fdfe7bbe35595da
seanbsu <122116208+seanbsu@users.noreply.github.com>
2023-11-27 10:12:38 -0700

    adding more tests for the State class

--- test/turingMachine/TestTM.java
+++ test/turingMachine/TestTM.java
@@ -5,6 +5,8 @@ import turingMachine.State;
 import turingMachine.Transition;
 import turingMachine.Tape;
 
+import java.util.ArrayList;
+
 import static org.junit.Assert.*;
 public class TestTM {
 
@@ -23,6 +25,60 @@ public class TestTM {
         assertEquals("1", s.toString());
         assertFalse(s.isAccept());
     }
+    @Test
+    public void testAddTransition() {
+        State state = new State(1);
+        Transition t1 = new Transition(1, 0, 'R', 3);
+        Transition t2 = new Transition(0, 1, 'L', 2);
+        ArrayList<Transition> transitionsList = new ArrayList<>();
+        transitionsList.add(t1);
+        state.setTransitions(transitionsList);
+        state.addTransition(t2);
+
+        ArrayList<Transition> transitions = state.getTransitions();
+        assertEquals(2, transitions.size());
+        assertTrue(transitions.contains(t1));
+        assertTrue(transitions.contains(t2));
+    }
+
+    @Test
+    public void testSetTransitions() {
+        State state = new State(1);
+        Transition t1 = new Transition(1, 0, 'R', 3);
+        Transition t2 = new Transition(0, 1, 'L', 2);
+
+        ArrayList<Transition> newTransitions = new ArrayList<>();
+        newTransitions.add(t1);
+        newTransitions.add(t2);
+
+        state.setTransitions(newTransitions);
+
+        ArrayList<Transition> transitions = state.getTransitions();
+        assertEquals(2, transitions.size());
+        assertTrue(transitions.contains(t1));
+        assertTrue(transitions.contains(t2));
+    }
+
+    @Test
+    public void testSetAccept() {
+        State state = new State(1);
+        assertFalse(state.isAccept());
+
+        state.setAccept(true);
+        assertTrue(state.isAccept());
+
+        state.setAccept(false);
+        assertFalse(state.isAccept());
+    }
+
+    @Test
+    public void testToString() {
+        State state = new State(5);
+        assertEquals("5", state.toString());
+    }
+
+// Add more tests as needed for specific functionality or edge cases
+
 
     @Test
     public void testPrivateNodeConstructor(){
