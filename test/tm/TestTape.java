97fb47a0e12c6b61d5d743c7acd5253b84af6498
kadendavis866 <kadendavis866@u.boisestate.edu>
2023-11-27 21:51:56 -0700

    add some tests for tape

--- test/tm/TestTape.java
+++ test/tm/TestTape.java
@@ -9,6 +9,88 @@ import tm.Tape;
 public class TestTape {
 
     @Test
+    public void testConstructor() {
+        String input = "1212312";
+        Tape t = new Tape(input);
+        assertEquals(input, t.toString());
+    }
+
+    @Test
+    public void testRightLeft() {
+        String input = "1212312";
+        Tape t = new Tape(input);
+        assertEquals(1, t.getValue());
+        t.moveRight();
+        assertEquals(2, t.getValue());
+        t.moveRight();
+        assertEquals(1, t.getValue());
+        t.moveRight();
+        assertEquals(2, t.getValue());
+        t.moveRight();
+        assertEquals(3, t.getValue());
+        t.moveLeft();
+        assertEquals(2, t.getValue());
+        t.moveLeft();
+        assertEquals(1, t.getValue());
+        t.moveRight();
+        assertEquals(2, t.getValue());
+        t.moveRight();
+        assertEquals(3, t.getValue());
+        t.moveRight();
+        assertEquals(1, t.getValue());
+        t.moveRight();
+        assertEquals(2, t.getValue());
+    }
+
+    @Test
+    public void testBiInfinite() {
+        String input = "123";
+        Tape t = new Tape(input);
+        for(int i = 0; i < input.length(); i++){
+            t.moveLeft();
+            assertEquals(0, t.getValue());
+        }
+        for(int i = 0; i < input.length(); i++){
+            t.moveRight();
+        }
+        for(int i = 0; i < input.length(); i++){
+            assertNotEquals(0, t.getValue());
+            t.moveRight();
+        }
+        for(int i = 0; i < input.length(); i++){
+            t.moveRight();
+            assertEquals(0, t.getValue());
+        }
+
+    }
+
+    @Test
+    public void testSetValue() {
+        String input = "1212312";
+        Tape t = new Tape(input);
+        int i = 0;
+        while (t.getValue() != 0) {
+            t.setValue((i % 3) + 1);
+            i++;
+            t.moveRight();
+        }
+
+        do {
+            t.moveLeft();
+        } while (t.getValue() != 0);
+        t.moveRight();
+
+        i = 0;
+        while (t.getValue() != 0) {
+            assertEquals((i % 3) + 1, t.getValue());
+            i++;
+            t.moveRight();
+        }
+
+        assertEquals("012312310", t.toString());
+    }
+
+    @Test
     public void testPrivateNodeConstructor() {
         Tape.NodeWrapper nw = new Tape.NodeWrapper(1);
         assertEquals(1, nw.getValue());
--- tm/Tape.java
+++ tm/Tape.java
@@ -4,12 +4,12 @@ package tm;
  * turingMachine.Tape class to represent the  bi-infinite tape of a Turing Machine
  */
 public class Tape {
-   private  Node curr, first, last;
+    private Node curr, first, last;
 
     /**
      * Constructor for an empty tape
      */
-    public Tape(){
+    public Tape() {
         curr = new Node(0);
         curr.left = curr.right = null;
         first = curr;
@@ -18,22 +18,24 @@ public class Tape {
 
     /**
      * Constructor for a tape with initial input
+     *
      * @param input the initial input on the tape
      */
-    public Tape(String input){
+    public Tape(String input) {
         this();
-        for(int i = 0; i < input.length();i++ ){
+        for (int i = 0; i < input.length(); i++) {
             this.setValue(Character.getNumericValue(input.charAt(i)));
             this.moveRight();
         }
+        if(curr.left != null) curr.left.right = null; // remove trailing node
         curr = first;
     }
 
     /**
      * Move the tape to the right
      */
-    public void moveRight(){
-        if(curr.right == null){
+    public void moveRight() {
+        if (curr.right == null) {
             curr.right = new Node(0);
             curr.right.left = curr;
             last = curr.right;
@@ -44,8 +46,8 @@ public class Tape {
     /**
      * Move the tape to the left
      */
-    public void moveLeft(){
-        if(curr.left == null){
+    public void moveLeft() {
+        if (curr.left == null) {
             curr.left = new Node(0);
             curr.left.right = curr;
             first = curr.left;
@@ -55,67 +57,78 @@ public class Tape {
 
     /**
      * Get the contents of the tape at the current position
+     *
      * @return the contents of the tape at the current position
      */
-    public int getValue(){
+    public int getValue() {
         return curr.value;
     }
 
     /**
      * Set the value for the contents of the tape at the current position
+     *
      * @param value the value to set the current position on the tape to.
      */
-    public void setValue(int value){
+    public void setValue(int value) {
         curr.value = value;
     }
 
     /**
      * Get the contents of the tape at the current position
      */
-    public void printTape(){
+    public String toString() {
         Node temp = this.first;
-        while(temp != null){
-            System.out.print(temp.value + " ");
+        StringBuilder sb = new StringBuilder();
+        while (temp != null) {
+            sb.append(temp.value);
             temp = temp.right;
         }
+        return sb.toString();
     }
 
     /**
      * Class to represent one "cell" of the tape
      */
-    private static class Node{
+    private static class Node {
         private int value;
         private Node left;
         private Node right;
-        public Node(int value){
+
+        public Node(int value) {
             this.value = value;
             this.left = null;
             this.right = null;
         }
 
-        private int getValue(){
+        private int getValue() {
             return value;
         }
 
-        private Node getLeft(){
+        private Node getLeft() {
             return left;
         }
-        private Node getRight(){
+
+        private Node getRight() {
             return right;
         }
     }
-    public static class NodeWrapper{
+
+    public static class NodeWrapper {
         private Node node;
-       public NodeWrapper(int val){
+
+        public NodeWrapper(int val) {
             node = new Node(val);
         }
-        public int getValue(){
+
+        public int getValue() {
             return node.getValue();
         }
-        public Node getLeft(){
+
+        public Node getLeft() {
             return node.getLeft();
         }
-        public Node getRight(){
+
+        public Node getRight() {
             return node.getRight();
         }
     }
